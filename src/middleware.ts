3a763561794e5c7f90a4de2fe24d91abbf9db50a
Ricardo <icardogoat@gmail.com>
2025-06-24 21:57:52 +0000

    agora vamos fazer o usraio so entra nas paginas admin se no banco de dad

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,16 @@
 import { withAuth } from "next-auth/middleware"
 
 export default withAuth({
+  callbacks: {
+    authorized: ({ req, token }) => {
+      // Routes that require admin role
+      if (req.nextUrl.pathname.startsWith("/admin")) {
+        return token?.admin === true
+      }
+      // For all other matched routes, just require the user to be logged in
+      return !!token
+    },
+  },
   pages: {
     signIn: "/",
   },
